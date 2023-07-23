2c0da363abcd0d3f01131d34e5865a555411a1cb
Ng Guoyou <gyng@users.noreply.github.com>
2023-07-24 02:27:26 +0800

    chore: eslint ignore ThemeInjector, add cache to build workflow

--- client/src/lib/components/NowPlaying/themes/ThemeInjector.svelte
+++ client/src/lib/components/NowPlaying/themes/ThemeInjector.svelte
@@ -3,6 +3,7 @@
 	export let html: string | undefined;
 </script>
 
-<!-- Need to break up <style> to bypass svelte preprocessor picking the tag up -->
+<!-- eslint-disable-next-line svelte/no-at-html-tags -->
 {@html `<` + `style>${css}</style>`}
+<!-- eslint-disable-next-line svelte/no-at-html-tags -->
 {@html html}
